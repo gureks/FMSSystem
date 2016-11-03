30546017d29c54076c4755b33fd335e2a7154c43
Gurek Singh <gurek15033@iiitd.ac.in>
2016-11-04 00:02:49 +0530

    added getter and setters to Task

--- Task.java
+++ Task.java
@@ -17,8 +17,33 @@ public class Task
 		this.status = status;
 		timestamp = new SimpleDateFormat("dd/MM/yyyy HH.mm.ss").format(new Date());
 	}
-	
-	public void recordTime()
+
+	public Date getDeadline()
+	{
+		return deadline;
+	}
+
+	public String getStatus()
+	{
+		return status;
+	}
+
+	public String getTimestamp()
+	{
+		return timestamp;
+	}
+
+	public void setDeadline(Date deadline)
+	{
+		this.deadline = deadline;
+	}
+
+	public void setStatus(String status)
+	{
+		this.status = status;
+	}
+
+	public void setTimestamp()
 	{
 		timestamp = new SimpleDateFormat("dd/MM/yyyy HH.mm.ss").format(new Date());
 	}
